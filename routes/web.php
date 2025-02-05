a7db58042080cffc271b1eb9ed5b0dcdbe2d422c
Edem <edem1pm@yandex.ru>
2025-02-05 20:14:22 +0300

    -измени дкоерфайл и файл nginx

--- routes/web.php
+++ routes/web.php
@@ -4,9 +4,8 @@
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/1', function () {
-    return view('welcome');
-});
+Route::get('/', [\App\Http\Controllers\HomeController::class, 'index'])->name('bee.index');
+
 Route::prefix('bee')->group(function () {
     Route::get('/', [\App\Http\Controllers\HomeController::class, 'index'])->name('bee.index');
 });
